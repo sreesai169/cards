2ac63a5e50a29c446bfbea0273a9dd9ee905a740
sreesai169 <sreesai169@gmail.com>
2020-07-15 12:31:59 +0530

    Updates

---
+++ precache-manifest.06d9e3a4f21c833b7d54096f33c0e86d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ae29fb84d9bc184d0485f59372f7903",
+    "url": "/sreesai169/cards/index.html"
+  },
+  {
+    "revision": "f56cbd3910cd33b4e66d",
+    "url": "/sreesai169/cards/static/css/2.326c04ff.chunk.css"
+  },
+  {
+    "revision": "2eb68766536f07e4bc81",
+    "url": "/sreesai169/cards/static/css/main.1d3fa62e.chunk.css"
+  },
+  {
+    "revision": "f56cbd3910cd33b4e66d",
+    "url": "/sreesai169/cards/static/js/2.b0227dcb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sreesai169/cards/static/js/2.b0227dcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2eb68766536f07e4bc81",
+    "url": "/sreesai169/cards/static/js/main.ea697578.chunk.js"
+  },
+  {
+    "revision": "4769cddd5188247a91f2",
+    "url": "/sreesai169/cards/static/js/runtime-main.9caff11a.js"
+  },
+  {
+    "revision": "630602c4f59aeacbced92175ccb7b2a4",
+    "url": "/sreesai169/cards/static/media/apssdc.630602c4.jpg"
+  }
+]);
